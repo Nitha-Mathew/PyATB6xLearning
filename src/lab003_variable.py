eba80f99f2e9d7e610cb02df6aee4dc7ed22bb46
Nitha Mathew <nithamariamathew@gmail.com>
2025-10-13 09:38:26 +0300

    commiting type of variable file

---
+++ src/lab003_variable.py
@@ -0,0 +1,14 @@
+_name="Nitha"
+print(type(_name))
+
+num=8
+print(type(num))
+
+num1=9.8
+print(type(num1))
+
+student=["Nitha","36","Kerala"]
+print(type(student))
+
+student1={"Name":"Nitha","Age":36,"State":"Kerala"}
+print(type(student1))
